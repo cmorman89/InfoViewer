a477d5f482c3843ba9a45070d7d0659b4641a90d
Charles Morman <github@cmorman.com>
2023-10-15 17:43:30 -0700

    New nested panel layout, "Major" input working

--- src/InfoFrame.java
+++ src/InfoFrame.java
@@ -21,15 +21,22 @@ import java.awt.event.ActionEvent;
 /* Info Frame */
 public class InfoFrame extends JFrame{
     /* Window Dimensions */
-    private final int WINDOW_WIDTH  = 400;
-    private final int WINDOW_HEIGHT = 600;
+    private final int WINDOW_WIDTH  = 370;
+    private final int WINDOW_HEIGHT = 500;
 
     /* GUI Objects (Indented = Nested/Grouped/Contains) */
     private JPanel          displayPanel;
-        private JLabel          displayTitleLabel;
-        private JLabel          displayNameLabel;
-        private JLabel          displayMajorLabel;
-        private JLabel          displayMaritalStatusLabel;
+        private JPanel          displayNorthPanel;
+            private JLabel          displayTitleLabel;
+            private JPanel          displayBorderPanel;
+                private JPanel          displayInfoTitlePanel;
+                    private JLabel          displayNameTitleLabel;
+                    private JLabel          displayMajorTitleLabel;
+                    private JLabel          displayMaritalStatusTitleLabel;
+                private JPanel          displayInfoPanel;
+                    private JLabel          displayNameLabel;
+                    private JLabel          displayMajorLabel;
+                    private JLabel          displayMaritalStatusLabel;
     private JPanel          inputPanel;
         private JPanel          nameInputPanel;
             private JLabel          nameInputLabel;
@@ -63,20 +70,47 @@ public class InfoFrame extends JFrame{
     /* Action Listener: Update GUI Display on Change*/
     class InputListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
-            displayNameLabel.setText(nameInputTextField.getText());
+            //displayNameLabel.setText(nameInputTextField.getText());
             displayMajorLabel.setText(majorInputComboBox.getSelectedItem().toString());
-            displayMaritalStatusLabel.setText(maritalStatusInputRadioGroup.getSelection().toString());
+            //displayMaritalStatusLabel.setText(maritalStatusInputRadioGroup.getSelection().toString());
         }
     }
 
     /* Create Display Panel
      * @return JPanel to display selected information */
     public JPanel displayPanel() {
-        displayPanel = new JPanel(new GridLayout(4,1));
+        /* Panel Parameters */
+        final int GRID_ROWS     = 2;
+        final int GRID_COLS     = 1;
+        final int SUBGRID_ROWS  = 3;
+        final int SUBGRID_COLS  = 1;
+        final String COL_GAP    = " ".repeat(4);
+        /* Create Panel and SubPanels */
+        displayPanel            = new JPanel(new BorderLayout());
+        displayNorthPanel       = new JPanel(new GridLayout(GRID_ROWS, GRID_COLS));
+        displayBorderPanel      = new JPanel(new BorderLayout());
+        displayInfoTitlePanel   = new JPanel(new GridLayout(SUBGRID_ROWS, SUBGRID_COLS));
+        displayInfoPanel        = new JPanel(new GridLayout(SUBGRID_ROWS,SUBGRID_COLS));
         /* Display Panel Contents */
-
-
-
+            displayTitleLabel               = new JLabel(COL_GAP + "User Information:");
+            displayNameTitleLabel           = new JLabel(COL_GAP + "Name:" + COL_GAP);
+            displayNameLabel                = new JLabel(" - - - ");
+            displayMajorTitleLabel          = new JLabel(COL_GAP + "Major:" + COL_GAP);
+            displayMajorLabel               = new JLabel(" - - - ");
+            displayMaritalStatusTitleLabel  = new JLabel(COL_GAP + "Marital Status:" + COL_GAP);
+            displayMaritalStatusLabel       = new JLabel(" - - - ");
+        /* Panel Layout */
+            /* NORTH    */   displayPanel.add(displayNorthPanel, BorderLayout.NORTH);
+                /* Row 1 */     displayNorthPanel.add(displayTitleLabel);
+                /* Row 2 */     displayNorthPanel.add(displayBorderPanel);
+                    /* WEST     */   displayBorderPanel.add(displayInfoTitlePanel, BorderLayout.WEST);
+                        /* Row 1 */     displayInfoTitlePanel.add(displayNameTitleLabel);
+                        /* Row 2 */     displayInfoTitlePanel.add(displayMajorTitleLabel);
+                        /* Row 3 */     displayInfoTitlePanel.add(displayMaritalStatusTitleLabel);
+                    /* CENTER   */   displayBorderPanel.add(displayInfoPanel, BorderLayout.CENTER);
+                        /* Row 1 */     displayInfoPanel.add(displayNameLabel);
+                        /* Row 2 */     displayInfoPanel.add(displayMajorLabel);
+                        /* Row 3 */     displayInfoPanel.add(displayMaritalStatusLabel);
 
         return displayPanel;
     }
@@ -84,50 +118,75 @@ public class InfoFrame extends JFrame{
     /* Create Input Panel
      * @return JPanel to hold input subpanels */
     public JPanel inputPanel() {
-        inputPanel = new JPanel(new GridLayout(3,1));
-
-        /* Input Panel Contents */
-        inputPanel.add(nameInputPanel());
-        inputPanel.add(majorInputPanel());
-        inputPanel.add(maritalStatusInputPanel());
-
+        /* Panel Parameters */
+        final int GRID_ROWS = 3;
+        final int GRID_COLS = 1;
+        /* Create Panel */
+        inputPanel = new JPanel(new GridLayout(GRID_ROWS, GRID_COLS));
+        /* Input Panel Contents Grid */
+            /* Row 1 Col 1 */   inputPanel.add(nameInputPanel());
+            /* Row 2 Col 1 */   inputPanel.add(majorInputPanel());
+            /* Row 3 Col 1 */   inputPanel.add(maritalStatusInputPanel());
+        /* Return Panel */
         return inputPanel;
     }
 
     /* Create Name Input Subpanel
      * @return JPanel to hold name input objects */
     public JPanel nameInputPanel() {
+        /* Panel Parameters */
+        final String PANEL_TITLE = "Name";
+        /* Create Panel */
         nameInputPanel = new JPanel();
-        nameInputPanel.setBorder(new TitledBorder(new EtchedBorder(), "Name"));
+        nameInputPanel.setBorder(new TitledBorder(new EtchedBorder(), PANEL_TITLE));
         /* Name Input Panel Contents */
 
 
 
-
+        /* Return Panel */
         return nameInputPanel;
     }
 
     /* Create Major Input Subpanel
      * @return JPanel to hold major input objects */
     public JPanel majorInputPanel() {
+        /* Panel Parameters */
+        final String PANEL_TITLE = "Major";
+        /* Array of Majors */
+        String[] majorArray = {
+            "Art",
+            "Biology",
+            "Business",
+            "Chemistry",
+            "Communication",
+            "Computer Science",
+            "Mathematics",
+            "Psychology"
+        };
+        /* Create Panel */
         majorInputPanel = new JPanel();
-        majorInputPanel.setBorder(new TitledBorder(new EtchedBorder(), "Major"));
+        majorInputPanel.setBorder(new TitledBorder(new EtchedBorder(), PANEL_TITLE));
         /* Major Input Panel Contents */
-
-
-
+        majorInputComboBox = new JComboBox<String>(majorArray);
+            majorInputComboBox.addActionListener(onChange);
+            majorInputPanel.add(majorInputComboBox);
+        /* Return Panel */
         return majorInputPanel;
     }
 
     /* Create Marital Status Input Subpanel
      * @return JPanel to hold marital status input objects */
     public JPanel maritalStatusInputPanel() {
-        maritalStatusInputPanel = new JPanel(new GridLayout(1,3));
-        maritalStatusInputPanel.setBorder(new TitledBorder(new EtchedBorder(), "Marital Status"));
+        /* Panel Parameters */
+        final String    PANEL_TITLE = "Marital Status";
+        final int       GRID_ROWS   = 1;
+        final int       GRID_COLS   = 3;
+        /* Create Panel */
+        maritalStatusInputPanel = new JPanel(new GridLayout(GRID_ROWS, GRID_COLS));
+        maritalStatusInputPanel.setBorder(new TitledBorder(new EtchedBorder(), PANEL_TITLE));
         /* Marital Status Input Panel Contents */
-
-
-
+        maritalStatusInputRadioGroup = new ButtonGroup();
+        /* Return Panel */
         return maritalStatusInputPanel;
     }
 }
